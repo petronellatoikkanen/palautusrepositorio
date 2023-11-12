0a2eee5e71fc346a042ff84596763c05d12f2ed8
Elviira <petronella.toikkanen@gmail.com>
2023-11-12 14:54:35 +0200

    10-12

--- viikko2/nhl-reader/src/index.py
+++ viikko2/nhl-reader/src/index.py
@@ -1,7 +1,18 @@
+import requests
 from player import Player
+from player_reader import PlayerReader
+from player_stats import PlayerStats
+
 
 def main():
-    pass
+    url = "https://studies.cs.helsinki.fi/nhlstats/2022-23/players"
+    reader = PlayerReader(url)
+    players = reader.get_players()
+    stats = PlayerStats(players)
+    players_list = stats.top_scorers_by_nationality("FIN")
+    
+    for player in players_list:
+        print(player)
 
 if __name__ == "__main__":
     main()
