b243e7d9de4cecab03c549aceeddd467cd937131
Elviira <petronella.toikkanen@gmail.com>
2023-11-12 14:55:35 +0200

    testi

--- viikko1/nhl-statistics-1/src/index.py
+++ viikko1/nhl-statistics-1/src/index.py
@@ -3,7 +3,7 @@ from player_reader import PlayerReader, PlayerReaderStub
 
 
 def main():
-    stats = StatisticsService(PlayerReaderStub())
+    stats = StatisticsService(PlayerReader())
     philadelphia_flyers_players = stats.team("PHI")
     top_scorers = stats.top(10)
 
